cacfee7184f595e6aefbe8b5ab6a6580a4cfb1d2
swapnilgupta14 <swapnilguptaswag@gmail.com>
2025-03-06 22:07:44 +0530

    Update axios instance baseURL for production deployment

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5001", // Local development server
+  baseURL: "https://marieta-express-backend-1.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
 });
 
 export default axiosInstance;
+
+// baseURL: "http://localhost:5001"
