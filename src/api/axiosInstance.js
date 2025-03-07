8eecc0250b28ce1087da59502d5bdd6339b05209
Ninecamp <meenal@ninecamp.co>
2025-03-07 15:43:41 +0530

    Update axiosInstance.js

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://marieta-express-backend-1.onrender.com",
+  baseURL: "https://marieta-express-backend-25c6.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
